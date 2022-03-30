bd20e6e84b00830d71dc1bf48488d9775a243362
Suezo <suezojoshua@gmail.com>
2022-03-30 08:02:06 +0800

    Camera function added in SelfieActivity

--- app/src/main/java/com/year3project/qrcodegenerator/SelfieActivity.java
+++ app/src/main/java/com/year3project/qrcodegenerator/SelfieActivity.java
@@ -1,10 +1,26 @@
 package com.year3project.qrcodegenerator;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class SelfieActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    Button openBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +28,32 @@ public class SelfieActivity extends AppCompatActivity {
         setContentView(R.layout.activity_selfie);
 
         getSupportActionBar().hide();
+
+        imageView = findViewById(R.id.image_view);
+        openBtn = findViewById(R.id.open_btn);
+
+        if (ContextCompat.checkSelfPermission(SelfieActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(SelfieActivity.this,
+                    new String[]{
+                            Manifest.permission.CAMERA
+                    }, 100);
+        }
+
+        openBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode ==  100 ) {
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(captureImage);
+        }
     }
 }
